e9cc79064207d31d0dd789e028b3a640693d5938
BryantH2003 <88125614+BryantH2003@users.noreply.github.com>
2025-07-27 11:51:46 -0500

    Added Intents

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import webserver
 
 DISCORD_TOKEN = os.environ['discordkey']
 
-client = commands.Bot(command_prefix = '!')
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix = '!', intents=intents)
 
 @client.event
 async def on_ready():
